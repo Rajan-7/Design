e54f0455c1fa7a7e17c6f291a965070bb925aca5
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-04-20 22:27:31 +0545

    ft:initialized design project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className="text-secondary">Hello World !!</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
