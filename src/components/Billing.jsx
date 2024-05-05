5936b377113c69c0f889d2e665f7dba6a4aa7d7a
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-05-05 21:16:49 +0545

    ft:Billing.jsx,CardDeal.jsx complete

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,5 +1,42 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="h-[100%] w-[100%] z-[5] relative"
+        />
+        <div className="z-[3] absolute -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="z-[0] absolute -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" />
+          billing & invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6 ">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] mr-5 object-contain cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_store"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ style }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${style}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${style} rounded-[10px]`}
     >
       Get Started
     </button>
