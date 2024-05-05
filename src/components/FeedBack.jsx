2b9c66694a4545a2fbfdf4c93a3f0604865b1fd3
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-05-05 21:49:05 +0545

    ft:Testomonial.jsx complete

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -1,10 +1,32 @@
+import { quotes } from "../assets";
 
-const FeedBack = () => {
+const FeedBack = ({ content, title, img, name }) => {
   return (
-    <div>
-      FeedBack
-    </div>
-  )
-}
+    <div
+      className="flex justify-between flex-col 
+       max-w-[370px] px-10 py-12
+       md:mr-10 sm:mr-5 mr-0 my-5 
+       rounded-[20px]
+       feedback-card"
+    >
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="h-[27px] w-[42px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[18px] leading-[32px] text-white ml-4 my-10">
+        {content}
+      </p>
 
-export default FeedBack
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="h-[48px] w-[48px] rounded-full" />
+        <div className="flex flex-col">
+          <h4 className="font-poppins font-semibold text-[20px] leading-[32px] ml-4 text-white">{name}</h4>
+          <p className="font-poppins font-semibold text-[16px] leading-[24px] ml-4 text-dimWhite">{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedBack;
--- src/components/Testomonial.jsx
+++ src/components/Testomonial.jsx
@@ -1,5 +1,44 @@
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedBack from "./FeedBack";
+
 const Testomonial = () => {
-  return <div>Testomonial</div>;
+  return (
+    <section
+      id="clients"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      <div className="absolute z-[0] h-[60%] w-[60%] -right-[50%] rounded-full blue__gradient"/>
+      <div
+        className="w-full flex justify-between 
+        items-center relative z-[1] 
+        md:flex-row flex-col 
+        sm:mb-16 mb-6"
+      >
+        <h1 className={styles.heading2}>
+          What people are <br className="sm:block hidden" />
+          saying about us
+        </h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div
+        className="flex flex-wrap
+         sm:justify-start justify-center w-full 
+         feedback-container 
+         relative z-[1]"
+      >
+        {feedback.map((card)=>(
+          <FeedBack key={card.id} {...card}/>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testomonial;
