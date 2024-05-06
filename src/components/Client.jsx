e893d3c75d7c2ad1f1393adb9e5e737e119176d6
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-05-06 20:28:39 +0545

    ft:complete project

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,9 +1,19 @@
+import { clients } from "../constants";
+import styles from '../style';
 
 const Client = () => {
   return (
-    <div>
-      CLient
-    </div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {
+          clients.map((client)=>(
+            <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px]  min-w-[120px]`}>
+              <img src={client.logo} alt="client_logo" className="sm:w-[192px] mb-4 w-[100px] object-contain" />
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
